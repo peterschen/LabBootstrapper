fccd6d26803a7feab350824c21f9c5440fd84acf
Christoph Petersen <cp@peterschen.de>
2016-10-31 22:32:46 +0100

    Added installation of OM

--- LabBootstrapper/Configurations/ConfigurationOM.ps1
+++ LabBootstrapper/Configurations/ConfigurationOM.ps1
@@ -14,7 +14,9 @@ configuration ConfigurationOM
 
     Import-DscResource -ModuleName PSDesiredStateConfiguration,
         @{ModuleName="xNetworking";ModuleVersion="2.11.0.0"},
-        @{ModuleName="xComputerManagement";ModuleVersion="1.8.0.0"} 
+        @{ModuleName="xComputerManagement";ModuleVersion="1.8.0.0"},
+        @{ModuleName="xCredSSP";ModuleVersion="1.1.0.0"},
+        @{ModuleName="xSCOM";ModuleVersion="1.3.3.0"}
 
     $domainPrefix = $DomainName.Split(".")[0];
 
@@ -22,6 +24,10 @@ configuration ConfigurationOM
     );
 
     $domainCredential = New-Object System.Management.Automation.PSCredential ("$domainName\Administrator", $Credential.Password);
+    $actionCredential = New-Object System.Management.Automation.PSCredential ("$domainName\s-om-msaa", $Credential.Password);
+    $sdkCredential = New-Object System.Management.Automation.PSCredential ("$domainName\s-om-sdk", $Credential.Password);
+    $drCredential = New-Object System.Management.Automation.PSCredential ("$domainName\s-om-datareader", $Credential.Password);
+    $dwCredential = New-Object System.Management.Automation.PSCredential ("$domainName\s-om-datawriter", $Credential.Password);
 
     Node OM
     {
@@ -108,6 +114,73 @@ configuration ConfigurationOM
             Credential = $domainCredential
             MembersToInclude = "$DomainName\g-RemoteManagementUsers"
             DependsOn = "[xComputer]C-JoinDomain"
+        }
+
+        Package "P-SqlServerClrTypes"
+        {
+            Ensure = "Present"
+            Name = "Microsoft System CLR Types for SQL Server 2014"
+            ProductId = ""
+            Path = "C:\LabBits\prereqs\SQLSysClrTypes.msi"
+            Arguments = "ALLUSERS=2"
+        }
+
+        Package "P-ReportViewer"
+        {
+            Ensure = "Present"
+            Name = "Microsoft Report Viewer 2015 Runtime"
+            ProductID = ""
+            Path = "C:\LabBits\prereqs\ReportViewer.msi"
+            Arguments = "ALLUSERS=2"
+            DependsOn = "[Package]P-SqlServerClrTypes"
+        }
+
+        xCredSSP "CS-Server"
+        {
+            Ensure = "Present"
+            Role = "Server"
+        }
+
+        xCredSSP "CS-Client"
+        {
+            Ensure = "Present"
+            Role = "Client"
+            DelegateComputers = $Node.NodeName
+        }
+
+        WaitForAll "WFA-DB"
+        {
+            NodeName = "DB"
+            ResourceName = "[xSqlServerFirewall]SSF-Firewall"
+            PsDscRunAsCredential = $domainCredential
+            RetryCount = 720
+            RetryIntervalSec = 5
+        }
+
+        xSCOMManagementServerSetup "SMSS-ManagementServer"
+        {
+            Ensure = "Present"
+            SourcePath = "C:\LabBits"
+            SourceFolder = "Source"
+            SetupCredential = $domainCredential
+            ManagementGroupName = "$domainPrefix"
+            FirstManagementServer = $true
+            ActionAccount = $actionCredential
+            DASAccount = $sdkCredential
+            DataReader = $drCredential
+            DataWriter = $dwCredential
+            SqlServerInstance = "DB"
+            DwSqlServerInstance = "DB"
+            DependsOn = "[xComputer]C-JoinDomain", "[xCredSSP]CS-Server", "[xCredSSP]CS-Client", "[Package]P-ReportViewer", "[WaitForAll]WFA-DB"
+        }
+
+        xSCOMConsoleSetup "SCS-Console"
+        {
+            Ensure = "Present"
+            SourcePath = "C:\LabBits"
+            SourceFolder = "Source"
+            SetupCredential = $domainCredential
+            DependsOn = "[xSCOMManagementServerSetup]SMSS-ManagementServer"
         }
     }
 }
