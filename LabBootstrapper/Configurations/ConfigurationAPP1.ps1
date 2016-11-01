e7adddc3153428b2fe1880c97e12c432a616bf2b
Christoph Petersen <cp@peterschen.de>
2016-11-01 09:32:58 +0100

    Added configuration for container application server

---
+++ LabBootstrapper/Configurations/ConfigurationAPP1.ps1
@@ -0,0 +1,114 @@
+configuration ConfigurationAPP1
+{
+    param 
+    ( 
+        [Parameter(Mandatory = $true)]
+        [string] $DomainName,
+
+        [Parameter(Mandatory = $true)]
+        [pscredential] $Credential,
+
+        [Parameter(Mandatory = $true)]
+        [string] $NetworkPrefix
+    );
+
+    Import-DscResource -ModuleName PSDesiredStateConfiguration,
+        @{ModuleName="xNetworking";ModuleVersion="2.11.0.0"},
+        @{ModuleName="xComputerManagement";ModuleVersion="1.8.0.0"} 
+
+    $domainPrefix = $DomainName.Split(".")[0];
+
+    $features = @(
+        "Containers"
+    );
+
+    $domainCredential = New-Object System.Management.Automation.PSCredential ("$domainName\Administrator", $Credential.Password);
+
+    Node APP1
+    {
+        foreach($feature in $features)
+        {
+            WindowsFeature "WF-$feature" 
+            { 
+                Name = $feature
+                Ensure = "Present"
+            }
+        }
+
+        xFirewall "F-FPS-NB_Datagram-In-UDP"
+        {
+            Name = "FPS-NB_Datagram-In-UDP"
+            Ensure = "Present"
+            Enabled = "True"
+        }
+
+        xFirewall "F-FPS-NB_Name-In-UDP"
+        {
+            Name = "FPS-NB_Name-In-UDP"
+            Ensure = "Present"
+            Enabled = "True"
+        }
+
+        xFirewall "F-FPS-NB_Session-In-TCP"
+        {
+            Name = "FPS-NB_Session-In-TCP"
+            Ensure = "Present"
+            Enabled = "True"
+        }
+
+        xFirewall "F-FPS-SMB-In-TCP"
+        {
+            Name = "FPS-SMB-In-TCP"
+            Ensure = "Present"
+            Enabled = "True"
+        }
+
+        xIPAddress "IA-Ip"
+        {
+            IPAddress = "$NetworkPrefix.70"
+            SubnetMask = 24
+            InterfaceAlias = "Ethernet"
+            AddressFamily = "IPv4"
+        }
+
+        xDnsServerAddress "DSA-DnsConfiguration"
+        { 
+            Address = "$NetworkPrefix.10"
+            InterfaceAlias = "Ethernet"
+            AddressFamily = "IPv4"
+            DependsOn = "[xIPAddress]IA-Ip"
+        }
+
+        xComputer "C-JoinDomain"
+        {
+            Name = $Node.NodeName
+            DomainName = $DomainName
+            Credential = $domainCredential
+            DependsOn = "[xDnsServerAddress]DSA-DnsConfiguration"
+        }
+
+        Group "G-Administrators"
+        {
+            GroupName = "Administrators"
+            Credential = $domainCredential
+            MembersToInclude = "$DomainName\g-LocalAdmins"
+            DependsOn = "[xComputer]C-JoinDomain"
+        }
+
+        Group "G-RemoteDesktopUsers"
+        {
+            GroupName = "Remote Desktop Users"
+            Credential = $domainCredential
+            MembersToInclude = "$DomainName\g-RemoteDesktopUsers"
+            DependsOn = "[xComputer]C-JoinDomain"
+        }
+
+        Group "G-RemoteManagementUsers"
+        {
+            GroupName = "Remote Management Users"
+            Credential = $domainCredential
+            MembersToInclude = "$DomainName\g-RemoteManagementUsers"
+            DependsOn = "[xComputer]C-JoinDomain"
+        }
+    }
+}
