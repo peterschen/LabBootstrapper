6eeb96a1a00fabd3455d06dd377e41520d40c07b
Christoph Petersen <cp@peterschen.de>
2016-11-02 05:53:31 +0100

    Added script to initialize docker swam demo

---
+++ LabBootstrapper/Scripts/Initialize-SwarmDemo.ps1
@@ -0,0 +1,15 @@
+﻿$env:DOCKER_HOST="tcp://10.4.0.70:2375";
+
+# Cleanup
+docker restart swarm-master | Out-Null
+Write-Host "Master restarted";
+docker rm -vf portainer | Out-Null
+Write-Host "Portainer deleted";
+
+# Init
+$swarmMasterIp = $(docker inspect --format '{{ .NetworkSettings.Networks.nat.IPAddress }}' swarm-master);
+docker run -d --name=portainer --restart=always -p 80:9000 portainer/portainer:windows -H tcp://$($swarmMasterIp):2375 --swarm | Out-Null
+Write-Host "Portainer started";
+
+$env:DOCKER_HOST="tcp://10.4.0.70:3375";
+Write-Host "Docker environment set: $env:DOCKER_HOST";
